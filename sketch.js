52bf75ebae4d43dbe71f5803f474b24c7c754ff1
omkar <87514785+9420873305@users.noreply.github.com>
2021-08-05 14:53:21 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+ 
+var ship
+
+function preload(){
+seaImg = loadImage("sea.png");
+shipImg1=loadAnimation(//list the naes the file that you want to load)
+}
+
+function setup(){
+  createCanvas(400,400);
+ spriteName.addImg(seaImg); 
+}
+
+function draw() {
+  background("blue");
+ //code to reset the background
+ if (sea.x<0){
+sea.x=sea.width/2;
+
+
+ }
+}
